adf50f8f312ab6e13bc6b3370c2c94ce273e689d
Kangwook Lee <43847427+kvngwxxk@users.noreply.github.com>
2025-01-14 16:28:20 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "SwiftyStarRatingView",
+    platforms: [
+        .iOS(.v11) // 최소 지원 플랫폼
+    ],
+    products: [
+        .library(
+            name: "SwiftyStarRatingView",
+            targets: ["SwiftyStarRatingView"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SwiftyStarRatingView",
+            path: "Sources", // 소스 코드 디렉토리
+            exclude: ["SwiftyStarRatingView.podspec"],
+            publicHeadersPath: "."
+        )
+    ]
+)

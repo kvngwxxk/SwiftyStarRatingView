0f74575075b5c89f0480a193f5d460a3058d712f
Kangwook Lee <43847427+kvngwxxk@users.noreply.github.com>
2025-01-14 16:29:09 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .target(
             name: "SwiftyStarRatingView",
-            path: "Sources", // 소스 코드 디렉토리
+            path: "SwiftyStarRatingView", // 소스 코드 디렉토리
             exclude: ["SwiftyStarRatingView.podspec"],
             publicHeadersPath: "."
         )
